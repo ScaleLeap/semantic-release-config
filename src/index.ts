ad8007f1a27ca353b3b1f9b20323f5c9b80c90ea
Roman Filippov <rf@romanfilippov.com>
2021-05-29 15:56:00 +0700

    fix: changelog header

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,10 @@ const plugins: Plugin[] = [
     '@semantic-release/changelog',
     {
       changelogTitle: `
-          # Changelog
+# Changelog
 
-          All notable changes to this project will be documented in this file.
-          See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
         `.trim(),
     },
   ],
